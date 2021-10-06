a052fe593d32df4adc8c2539e968389afd3b3f01
NasirZBC <nasi0067@zbc.dk>
2021-10-06 08:50:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jed_mind_tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
